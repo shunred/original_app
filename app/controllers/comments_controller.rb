b9660597bd058bb4ac20d0074930f057fa8ea7f2
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-08-28 20:13:26 +0900

    コメント機能完成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,14 @@
 class CommentsController < ApplicationController
 
 	def create
-	  Comment.create(comment_params)
+		@comment = Comment.create(comment_params)
+		redirect_to "/tweets/#{@comment.tweet.id}"
 	end
 
 
    private
    def comment_params
-   	 params.require(:comment).permit(:text).merge(user_id: current_user.id)
+   	 params.require(:comment).permit(:text, :tweet_id).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
    end
 
 end
