5088553c5fecd0169bdb9956e4d5b9524331ccf8
Shunya akaboshi <shunyaakaboshi@ShunyanoMacBook-puro.local>
2018-09-22 13:24:18 +0900

    スクロールボタンの追加

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,20 @@
+
+$(function () {
+    var topBtn = $('#page-top');
+    topBtn.hide();
+    //スクロールが500に達したらボタン表示
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
+    });
+    //スルスルっとスクロールでトップへもどる
+    topBtn.click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 300);
+        return false;
+    });
+});
